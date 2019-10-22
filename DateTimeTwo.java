30d9d03a08796b2d4850a8035dbead7db8e3c5fb
Noah Stasyszen <stasyszen@gmail.com>
2019-10-21 23:10:28 -0500

    Initial commitment

---
+++ DateTimeTwo.java
@@ -0,0 +1,24 @@
+
+public class DateTimeTwo {
+
+	public void daysOfCurrentMonth() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void daysOfAnyMonth(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void compareYear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dateHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ MesoEquivalent.java
@@ -0,0 +1,13 @@
+
+public class MesoEquivalent {
+
+	public MesoEquivalent(String stId) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String calAsciiEqual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
