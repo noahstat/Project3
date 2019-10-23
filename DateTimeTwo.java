b12dbc4a603f5f058a8e3c6ee610ec35d02faf59
Noah Stasyszen <stasyszen@gmail.com>
2019-10-23 10:28:15 -0500

    Fixed the dates in the days of any month so year and month are correct

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -30,23 +30,28 @@ public class DateTimeTwo {
 
 	public void daysOfAnyMonth(int i, int j) {
 		Date givenDate = new Date();
-		givenDate.setDate(15);//since we want to find the last day
-		givenDate.setMonth(i);
-		givenDate.setYear(j);
-
+		givenDate.setDate(15);//since we want to find the fifteenth day
+		givenDate.setMonth(i - 1);
+		givenDate.setYear(j - 1900);
+		
 		SimpleDateFormat day = new SimpleDateFormat("EEEEEEEEE");
 		
 		String fifteenthDay = (day.format(givenDate)).toUpperCase();
 		
-		if(givenDate.getMonth() != 2)
+		//the months that have 31: september, april, june, november, and fubruary
+		if(givenDate.getMonth() != 1 || givenDate.getMonth() != 8 || givenDate.getMonth() != 3
+				|| givenDate.getMonth() != 5 || givenDate.getMonth() != 10)
 			givenDate.setDate(31);
-		else
+		else if(givenDate.getMonth() == 1) // make sure its not febuary (we're gonna act like leap year is fake)
 			givenDate.setDate(28);
+		else
+			givenDate.setDate(30);
 		
 		String lastDay = (day.format(givenDate)).toUpperCase();
 		
-		System.out.println("For the year (" + givenDate.getYear() + ") and month (" + givenDate.getMonth() + 
-				"), the fifteenth day is " + fifteenthDay + " and the last day is " + lastDay);
+		System.out.println("For the year (" + (givenDate.getYear() + 1900) + ") and month (" 
+				+ (givenDate.getMonth() + 1) + "), the fifteenth day is " + 
+				fifteenthDay + " and the last day is " + lastDay);
 	}
 
 	public void compareYear() {
